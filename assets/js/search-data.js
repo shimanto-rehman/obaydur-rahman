94a9b15e2d4f53b523e4fc42aa8917cbf891a0e4
shimanto-rehman <shimanto.rehman.bd@gmail.com>
2026-05-13 14:08:14 +0000

    Deploying to gh-pages from @ shimanto-rehman/obaydur-rahman@e7fb1f0a7c9354b6131b5c6c36e1018ad82e17b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-h1-s-m-obaydur-rahman-h1",
-    title: "<h1>S.M. Obaydur Rahman<h1/>",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/obaydur-rahman/";
